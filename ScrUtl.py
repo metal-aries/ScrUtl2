1f5859f52114cc4998418d2b34e84cfc00521453
moviemurby <r.okamura.061@ms.saitama-u.ac.jp>
2021-04-18 21:15:37 +0900

    キャンセルを押したら保存しない。プレビューの表示（途中）

--- ScrUtl.py
+++ ScrUtl.py
@@ -3,18 +3,30 @@ from PIL import ImageGrab
 from PIL import Image
 import tkinter
 from tkinter import messagebox
+import cv2
+import numpy
 
 # スクショを取得（書き換え予定）
-screenshot = ImageGrab.grabclipboard()
+screenshot = ImageGrab.grab()
+
+# tkinterの背景を削除
+root = tkinter.Tk()
+root.withdraw()
 
 # 取得した内容が画像であるか？
 if isinstance(screenshot, Image.Image):
-    # パスを指定し画像を保存（書き換え予定）
-    imagePath = path.abspath(__file__+"/..")
-    screenshot.save(imagePath+"/00.png")
+    cv2.imshow("画像の確認", numpy.asarray(screenshot))
+
+    answer = messagebox.askyesnocancel("画像の保存","aフォルダに保存しますか？")
+
+    if answer == tkinter.YES:
+        # パスを指定し画像を保存（書き換え予定）
+        imagePath = path.abspath(__file__+"/..")
+        screenshot.save(imagePath+"/00.png")
+    elif answer == tkinter.NO:
+        # パスを指定し画像を保存（書き換え予定）
+        imagePath = path.abspath(__file__+"/..")
+        screenshot.save(imagePath+"/01.png")
 else:
     # 画像でないことを通知
-    root = tkinter.Tk()
-    root.withdraw() # tkinterの背景を削除
-
     messagebox.showinfo(title="error", message="NoImage")
