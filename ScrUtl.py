ada659e14e33ed01db2f0d08401ffb5ae5abae38
moviemurby <r.okamura.061@ms.saitama-u.ac.jp>
2021-04-18 21:43:12 +0900

    サムネイルを表示

--- ScrUtl.py
+++ ScrUtl.py
@@ -6,6 +6,16 @@ from tkinter import messagebox
 import cv2
 import numpy
 
+##### 関数定義 #####
+def showImage(x: int, screenshot: Image.Image):
+    image = numpy.asarray(screenshot)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    y = int(x * (screenshot.size[1] / screenshot.size[0]))
+    image = cv2.resize(image, dsize=(x, y))
+    print(y)
+    cv2.imshow("thumbnail", image)
+
+##### 本文 #####
 # スクショを取得（書き換え予定）
 screenshot = ImageGrab.grab()
 
@@ -14,9 +24,11 @@ root = tkinter.Tk()
 root.withdraw()
 
 # 取得した内容が画像であるか？
-if isinstance(screenshot, Image.Image):
-    cv2.imshow("画像の確認", numpy.asarray(screenshot))
+if isinstance(screenshot.copy(), Image.Image):
+    # 画像プレビュー
+    showImage(320, screenshot)
 
+    # ダイアログ表示
     answer = messagebox.askyesnocancel("画像の保存","aフォルダに保存しますか？")
 
     if answer == tkinter.YES:
@@ -24,7 +36,6 @@ if isinstance(screenshot, Image.Image):
         imagePath = path.abspath(__file__+"/..")
         screenshot.save(imagePath+"/00.png")
     elif answer == tkinter.NO:
-        # パスを指定し画像を保存（書き換え予定）
         imagePath = path.abspath(__file__+"/..")
         screenshot.save(imagePath+"/01.png")
 else:
